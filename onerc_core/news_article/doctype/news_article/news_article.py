8ba93f8bfb745ed100a882c381ef7e722549f20c
k-neth <kneth46@gmail.com>
2026-05-12 08:28:57 +0300

    Added News and Blog modu# Please enter the commit message for your changes. Lines starting

---
+++ onerc_core/news_article/doctype/news_article/news_article.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2026, Kelvin Njenga and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class NewsArticle(Document):
+	pass
+
+
